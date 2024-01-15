633c56b97e2dfaba331aee9f7a033e4ae5aa1620
drone <drone@ubuntu.localdomain>
2024-01-15 13:39:38 +0300

    add relativebnumber

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -8,6 +8,8 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 
+vim.opt.relativenumber = true
+
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
